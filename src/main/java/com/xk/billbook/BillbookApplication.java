3c26cb6a44a64bf80e3a038785ee86d8d0618614
GSLMXK <532599835@qq.com>
2018-07-30 19:24:23 +0800

    xk001
    记账系统

---
+++ src/main/java/com/xk/billbook/BillbookApplication.java
@@ -0,0 +1,12 @@
+package com.xk.billbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillbookApplication.class, args);
+    }
+}
