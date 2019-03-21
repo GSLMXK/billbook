b2d31599cf82d63bd74f4a772e3830c518c5fdaf
GSLMXK <532599835@qq.com>
2019-03-21 21:09:37 +0800

    xk046
    bill接口修改

--- src/main/java/com/xk/lifebook/interfaceController/BillInterfaceController.java
+++ src/main/java/com/xk/lifebook/interfaceController/BillInterfaceController.java
@@ -1,5 +1,6 @@
 package com.xk.lifebook.interfaceController;
 
+import com.xk.lifebook.admin.model.Bill;
 import com.xk.lifebook.admin.service.BillManagerService;
 import com.xk.lifebook.admin.service.BillTypeMgrService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +26,7 @@ public class BillInterfaceController {
     BillManagerService billmgrService;
     @Autowired
     BillTypeMgrService billTypeMgrService;
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     /**'
      * 获取当月账单统计数据
      * @return
@@ -44,5 +48,29 @@ public class BillInterfaceController {
     public Map<String,Object> getBillType(HttpServletRequest request,Integer userId){
         Map<String,Object> typeList = billTypeMgrService.findByList(userId);
         return typeList;
+    }
+    /**'
+     * 保存账单
+     * @return
+     */
+    @RequestMapping("/saveBill")
+    @ResponseBody
+    public String saveBill(HttpServletRequest request,String name,String billTypeId, String money, String description, String billDate,Integer userId){
+        Integer result = 0;
+        Bill bill = new Bill();
+        bill.setCreatorId(userId);
+        bill.setName(name);
+        bill.setDescription(description);
+        bill.setBillTypeId(Integer.valueOf(billTypeId));
+        try {
+            bill.setBillDate(sdf.parse(billDate));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        result = billmgrService.insertBill(bill);
+        if(result!=null&&result>0){
+            return "Bill save success!";
+        }
+        return "Bill save failed";
     }
 }
